45ee74ab75b7b5178006bb051365fdabb96c31fd
gheorghe-cristi <gheorgherobert17@stud.ase.ro>
2022-03-28 21:15:33 +0300

    commit

---
+++ src/FizzBuzz.java
@@ -0,0 +1,24 @@
+public class FizzBuzz {
+    public FizzBuzz(){
+    }
+
+    public static void main(String[] args) {
+        for(int i =1;i<=100;++i){
+            if (i%3==0 && i%5==0){
+                System.out.println("FizzBuzz,");
+            } else if(i%7==0){
+                System.out.println("Rizz,");
+            } else if(i%11==0){
+                System.out.println("Jazz");
+            }
+            else if (i%3==0){
+                System.out.println("Fizz,");
+            }
+            else if (i%5==0){
+                System.out.println("Buzz,");
+            } else {
+                System.out.println(i+",");
+            }
+        }
+    }
+}
